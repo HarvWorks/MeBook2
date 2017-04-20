cb5cb7ae7c76ccbaf35852a99765beeb9846826f
Harvey Chui <chuiharvey@gmail.com>
2017-04-19 21:42:03 -0700

    further minor changes to get it to play well with the server

--- app/config/database.py
+++ app/config/database.py
@@ -16,7 +16,7 @@ class DevelopmentDBConfig(DBConfig):
     DB_PASSWORD = 'root'
     DB_DATABASE_NAME = 'user_dashboard'
     DB_HOST = 'localhost'
-    DB_PORT = 8889
+    DB_PORT = 3306
     """ unix_socket is used for connecting with MAMP. Take this out if you aren't using MAMP """
     # DB_OPTIONS = {
     #     'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock'
